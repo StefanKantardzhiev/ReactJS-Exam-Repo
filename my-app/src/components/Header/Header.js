f8024cb1ddda6ef789ce1573f8163336ecaaa903
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-03-29 15:21:03 +0200

    Exam-Workshop Login/Register + Home - Done

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,31 @@
+// import { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+
+// import { AuthContext } from '../../contexts/AuthContext';
+
+export const Header = () => {
+    // const { isAuthenticated, userEmail } = useContext(AuthContext);
+
+    return (
+        <header>
+            <h1><a href='/' className="home" to="/">GermanMechanics.de</a></h1>
+            <nav>
+                <a href='/catalog'>All Offers</a>
+                {/* {isAuthenticated && ( */}
+                <div id="user">
+                    <span>email</span>
+                    <a href="/create-game">Create Offer</a>
+                    <a href="/logout">Logout</a>
+                </div>
+                {/* )} */}
+
+                {/* {!isAuthenticated && ( */}
+                <div id="guest">
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                </div>
+                {/* )} */}
+            </nav>
+        </header>
+    );
+};
