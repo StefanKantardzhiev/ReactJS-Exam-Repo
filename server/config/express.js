353bdcf86a24609730720a5de58847979fd0a22a
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-04-01 15:43:04 +0200

    Exam-Workshop

--- server/config/express.js
+++ server/config/express.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const session = require('../middlewares/session');
 const trimBody = require('../middlewares/trimBody');
-
+const cors = require('cors')
 module.exports = (app) => {
     const hbs = handlebars.create({
         extname: '.hbs'
@@ -11,7 +11,7 @@ module.exports = (app) => {
 
     app.engine('.hbs', hbs.engine);
     app.set('view engine', '.hbs');
-
+    app.use(cors())
     app.use('/static', express.static('static')); // express boiler
     app.use(express.urlencoded({ extended: true }));
     app.use(trimBody());
